3823d35252e466ad09a2c93b1f8315a99afb9b95
Ikhwanirgi <117446211+Ikhwanirgi@users.noreply.github.com>
2024-07-12 16:41:28 +0700

    tambahlagi

--- resources/views/Hasil/index.blade.php
+++ resources/views/Hasil/index.blade.php
@@ -1,10 +1,14 @@
-@extends('layouts.index')
-@section('title','Hasil')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Saw Gula pasir  | @yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
 
-    
-@endsection
-
+<body>
 <x-app-layout>
     <x-slot name="header">
         <div class="flex  items-center justify-between">
@@ -44,3 +48,4 @@
         </div>
     </div>
 </x-app-layout>
+</body>
